832224297927fc16f35f1e342209922fe10ce126
andersdberg <anders.berg@noteable.io>
2023-01-27 13:35:27 -0700

    Update to latest version of notebook image with new features

--- kernels/datascience-notebook/run.sh
+++ kernels/datascience-notebook/run.sh
@@ -27,24 +27,24 @@ case $kernel_name in
         echo "Starting Python kernel"
         # https://docs.python.org/3/using/cmdline.html#envvar-PYTHONSTARTUP
         export PYTHONSTARTUP=~/.pythonrc
-        python -m ipykernel_launcher -f ${connection_file} --debug
+        exec python -m ipykernel_launcher -f ${connection_file} --debug
         ;;
 
     ir)
         echo "Starting R kernel"
-        R --slave -e "IRkernel::main()" --args  ${connection_file}
+        exec R --slave -e "IRkernel::main()" --args  ${connection_file}
         ;;
 
     julia | julia-1.6)
         echo "Starting Julia kernel"
         # project path necessary to keep julia form using its defaults
-        julia -i --color=yes --project=/etc/noteable/project /opt/julia/packages/IJulia/e8kqU/src/kernel.jl ${connection_file}
+        exec julia -i --color=yes --project=/etc/noteable/project /opt/julia/packages/IJulia/e8kqU/src/kernel.jl ${connection_file}
         ;;
 
     *)
         echo "Unrecognized '$kernel_name' kernel, falling back to Python"
         # https://docs.python.org/3/using/cmdline.html#envvar-PYTHONSTARTUP
         export PYTHONSTARTUP=~/.pythonrc
-        python -m ipykernel_launcher -f ${connection_file} --debug
+        exec python -m ipykernel_launcher -f ${connection_file} --debug
         ;;
 esac
