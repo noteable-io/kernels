8fcaed61cf9f790cbd2bdcccbb35b2f8454801c4
Kafonek <matt.kafonek@noteable.io>
2023-06-29 14:21:03 -0400

    rust base

---
+++ rust/secrets_helper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script helps inject Secrets into the Kernel environment.
+# The Vault Agent will volume mount files into the Kernel container
+# at /vault/secrets.  Noteable Secrets will be in .env suffix files.
+# We want to parse all those files and export them as environment variables
+# in the bash script that kicks off the Kernel (ipykernel_launcher etc).
+
+secrets_directory=${VAULT_SECRETS_PATH:-/vault/secrets}
+
+if [ -d "$secrets_directory" ]; then
+    for file in "$secrets_directory"/*.env; do
+        name=$(basename "$file" .env)
+        name=${name^^}  # Convert to uppercase
+        if ! declare -p "$name" > /dev/null 2>&1; then  # If variable is not set
+            content=$(cat "$file")
+            export "$name=$content"
+        fi
+    done
+fi
