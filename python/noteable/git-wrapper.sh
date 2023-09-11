1baf79fd09dff782c54bc69d6383c5c07dd179a2
Dave Shoup <dave.shoup@gmail.com>
2023-09-11 11:12:04 -0400

    check env var before wrapping git commands

--- python/noteable/git-wrapper.sh
+++ python/noteable/git-wrapper.sh
@@ -5,11 +5,15 @@
 # Allowed command list
 allowed_commands=( "commit" "pull" "push" "status" "diff" "add" "fetch" "log" "version" )
 
-# Check if the command is allowed
-# shellcheck disable=SC2076
-if [[ ! " ${allowed_commands[*]} " =~ " ${1} " ]]; then
-    echo "That git command is not allowed, contact support@noteable.io if you think this is a mistake."
-    exit 1
+# If the NTBL_IS_GIT_BACKED env var is "true" then we check to see if the command is allowed.
+# If the env var is not set or is not "true" then we just pass through to git.
+if [[ "${NTBL_IS_GIT_BACKED}" == "true" ]]; then
+    # Check if the command is allowed
+    # shellcheck disable=SC2076
+    if [[ ! " ${allowed_commands[*]} " =~ " ${1} " ]]; then
+        echo "That git command is not allowed, contact support@noteable.io if you think this is a mistake."
+        exit 1
+    fi
 fi
 
 # Otherwise pass through to git at /usr/bin/git
