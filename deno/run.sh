5dbed739558e113e641eda5f303a8cc61cdd23c2
Kyle Kelley <rgbkrk@gmail.com>
2023-09-13 07:20:40 -0700

    starter work on incorporating the deno kernel (#38)
    * starter work on incorporating the deno kernel
    
    * include jupyter and install the kernel just to shake it out
    
    * Revert db77647f87888b7e869fcaa5a48d6448577bba53
    
    ---------
    
    Co-authored-by: Dave Shoup <dave.shoup@gmail.com>

---
+++ deno/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -o nounset
+set -o errexit
+
+set -x
+
+echo "Local time: $(date)"
+
+set +x
+echo "Injecting Secrets into environment, echoing is turned off"
+# shellcheck disable=SC1091
+source /tmp/secrets_helper.sh
+echo "Done injecting Secrets, turning echoing back on"
+set -x
+
+
+echo "Starting Deno kernel"
+
+exec deno --unstable jupyter --conn /etc/noteable/connections/connection_file.json
