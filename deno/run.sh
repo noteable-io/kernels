a37500cdf44f2d9d1fd066fc276958344df14b29
Dave Shoup <dave.shoup@gmail.com>
2023-09-15 19:43:22 -0400

    swap install to image build

--- deno/run.sh
+++ deno/run.sh
@@ -14,13 +14,7 @@ source /tmp/secrets_helper.sh
 echo "Done injecting Secrets, turning echoing back on"
 set -x
 
-# install the deno kernelspec, which we don't technically need since we're using a volume-mounted connection file
-deno --unstable \
-    jupyter \
-    --install
-
 echo "Starting Deno kernel"
-
 exec deno --unstable \
     jupyter \
     --conn /etc/noteable/connections/connection_file.json \
