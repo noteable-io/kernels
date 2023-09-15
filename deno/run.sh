052d05c536ebbe4dec7ac8c6a057f284fa04b39a
Dave Shoup <dave.shoup@gmail.com>
2023-09-15 19:07:43 -0400

    add `jupyter --install` & `--kernel`

--- deno/run.sh
+++ deno/run.sh
@@ -14,7 +14,13 @@ source /tmp/secrets_helper.sh
 echo "Done injecting Secrets, turning echoing back on"
 set -x
 
+# install the deno kernelspec, which we don't technically need since we're using a volume-mounted connection file
+deno --unstable jupyter --install
 
 echo "Starting Deno kernel"
 
-exec deno --unstable jupyter --conn /etc/noteable/connections/connection_file.json
+exec deno \
+    jupyter \
+    --unstable \
+    --conn /etc/noteable/connections/connection_file.json \
+    --kernel
