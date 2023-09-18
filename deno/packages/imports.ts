8dc0cfe81fc1cce2cd7914fe85cf30bd7565eea9
Dave Shoup <dave.shoup@gmail.com>
2023-09-18 18:30:07 -0400

    add desired imports

---
+++ deno/packages/imports.ts
@@ -0,0 +1,16 @@
+import "https://deno.land/std/assert/mod.ts";
+import "https://deno.land/x/canvas@v1.4.1/mod.ts";
+import "https://deno.land/x/deno_dom/deno-dom-wasm-noinit.ts";
+import "https://deno.land/x/display/mod.ts";
+import "https://deno.land/x/skia_canvas/mod.ts";
+
+import "https://esm.sh/jsdom";
+import "https://esm.sh/twind";
+
+import "npm:apache-arrow";
+import "npm:d3";
+import "npm:express";
+import "npm:nodejs-polars";
+import "npm:react";
+import "npm:vega-lite-api";
+
