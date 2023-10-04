7c370114527c388eb74f4c4966ca13baa55bc31b
Kyle Kelley <rgbkrk@gmail.com>
2023-10-04 14:56:13 -0700

    update and pin dependencies in deno cache

--- deno/packages/deps.ts
+++ deno/packages/deps.ts
@@ -1,15 +1,19 @@
-import "https://deno.land/std/assert/mod.ts";
+import "https://deno.land/std@0.201.0/assert/mod.ts";
 import "https://deno.land/x/canvas@v1.4.1/mod.ts";
-import "https://deno.land/x/deno_dom/deno-dom-wasm-noinit.ts";
-import "https://deno.land/x/display/mod.ts";
-import "https://deno.land/x/skia_canvas/mod.ts";
+import "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm-noinit.ts";
+import "https://deno.land/x/display@v1.0.1/mod.ts";
+import "https://deno.land/x/skia_canvas@0.5.4/mod.ts";
 
-import "https://esm.sh/jsdom";
-import "https://esm.sh/twind";
+import "https://esm.sh/jsdom@22.1.0";
+import "https://esm.sh/twind@1.0.7";
+import "https://esm.sh/@observablehq/plot@0.6.11";
 
-import "npm:apache-arrow";
-import "npm:d3";
-import "npm:express";
-import "npm:nodejs-polars";
-import "npm:react";
-import "npm:vega-lite-api";
+import "npm:apache-arrow@13.0.0";
+import "npm:d3@7.8.5";
+import "npm:express@4.18.2";
+import "npm:nodejs-polars@0.8.2";
+import "npm:react@18.2.0";
+import "npm:vega-lite-api@5.6.0";
+import "npm:@observablehq/plot@0.6.11";
+
+import "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
